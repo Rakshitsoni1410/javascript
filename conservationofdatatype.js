ad6feb9ea11dd9dcd25965758939ba703fff8600
Rakshitson1410 <rakshitrsoni@gmail.com>
2024-06-24 18:59:46 +0530

    some changes

---
+++ conservationofdatatype.js
@@ -0,0 +1,15 @@
+// let  score = null
+// console.log(typeof score);
+// console.log(typeof (score));
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+let str1 = "good";
+let str2 = "  morning";
+
+
+let str3 = str1+str2;
+console.log(str3);
--- data_type.js
+++ data_type.js
@@ -1,5 +1,9 @@
 "use strict";
 //alert(3+4);
-console.log(6+6);
+//console.log(6+6);
 
  console.log("rr");
+ let name = "rakshit";
+ let age = 19;
+ let isloggedin =false;
+ console.log(typeof "");
