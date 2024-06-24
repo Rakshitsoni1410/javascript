dddbf723676c2b51d70d91284e145ae89da3c479
Rakshitson1410 <rakshitrsoni@gmail.com>
2024-06-24 19:45:53 +0530

    some changes

--- strig.js
+++ strig.js
@@ -3,3 +3,5 @@
  const sirname = "soni";
 // console.log(name + sirname + "value");
 console.log(`hello  y name is ${name} and my sir name is ${sirname}`);
+const gameName  = new String(`rakshit`);
+console.log(gameName);
