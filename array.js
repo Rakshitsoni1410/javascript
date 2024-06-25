58e57464ed0fdbf41d60d0e7460928a0e44988d9
Rakshitson1410 <rakshitrsoni@gmail.com>
2024-06-25 17:03:39 +0530

    some changes

---
+++ array.js
@@ -0,0 +1,23 @@
+const myArr =[0,1,2,2,3,5]
+const myHero = ["rmaj ","rishbh","rakshit"];
+const myArr3 =new Array(1,2,3);
+//console.log(myArr[0]);
+//array method
+/*myArr .push(6)
+myArr.push(64)
+myArr.pop()
+myArr.unshift(0);
+myArr.shift();
+console.log(myArr.includes(2));
+console.log(myArr.indexOf(0));
+const newArr1 =myArr.join()
+console.log(myArr);
+console.log(typeof newArr1);*/
+ //slice , splice
+ console.log("a",myArr);
+const myn1 =myArr.slice(1,3);
+
+console.log(myn1);
+console.log("b",myArr);
+const myn2 = myArr.splice(1,3);
+console.log(myn2);
