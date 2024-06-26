89b8850b24ac9dfbcb152f103d12e14b6e978a88
Rakshitson1410 <rakshitrsoni@gmail.com>
2024-06-27 00:46:59 +0530

    some changes

---
+++ Scope.js
@@ -0,0 +1,48 @@
+//var c =300
+let a =320
+
+if (true) {
+    let a = 10;
+const  b=9;
+//console.log("innner: ",a); 
+}
+
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+function one() {
+    const username ="rakshit"
+    function two() {
+        const website = "youtube"
+        console.log(username);
+    }
+    //console.log(website);
+   // two()
+
+}
+//one()
+if(true){
+    const username = "rakshit"
+    if (username ==="rakshit") {
+        const website = "youtube" 
+      //  console.log(username+website);
+    }
+   // console.log(website);
+}
+//console.log(username);
+
+//++++
+
+function addone(num) {
+    return num + 1;
+}
+
+console.log(addone(1));  // Output: 2
+
+
+
+const addtwo = function (num) {
+    return num + 2
+}
+console.log(addtwo(2510));
