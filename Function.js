1d9a8d97d326a56f25ddae682f329ded0919729e
Rakshitson1410 <rakshitrsoni@gmail.com>
2024-06-26 21:51:14 +0530

    changes

---
+++ Function.js
@@ -0,0 +1,32 @@
+
+
+function sayMyname(){
+    console.log("R");
+console.log("a");
+}
+//sayMyname()
+
+//function addtwono(no1,no2){
+  //console.log( no1+no2);
+//}
+
+function addtwono(no1,no2){
+   
+//let result= no1+no2
+//return result;
+return no1+no2
+
+}
+  
+const result = addtwono(3,5);
+//console.log("result",result);
+
+function loginUser(username  ="same"){
+    if (!undefined) {
+        console.log("place enter the username ");
+        return
+    }
+    return `${username}just logged in`
+}
+//console.log(loginUser("rakshit"));
+console.log(loginUser("raj"));
