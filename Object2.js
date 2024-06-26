e3fe79e822434a1d7664024d8f51aa7eac5cfd06
Rakshitson1410 <rakshitrsoni@gmail.com>
2024-06-26 14:58:02 +0530

    some changes

---
+++ Object2.js
@@ -0,0 +1,48 @@
+//const tinderUser = new Object();
+const tinderUser= {}
+ tinderUser.id ="Rr1410"
+ tinderUser.username= "rakhsu"
+ tinderUser.isloggedIn= false
+//console.log(tinderUser);
+const regularUser = {
+    email :"some@gmail.com",
+fullname :{
+    userfullname :{
+        firstname :"rakshu",
+        lastname :"soni"
+    }
+}
+
+}
+//console.log(regularUser.fullname.userfullname.firstname);
+const obj1 ={
+    1:"a",2:"B"
+}
+const obj2= {
+    3:"a",4:"b"
+}
+const obj4={
+    5:"f",4:"t"
+}
+//const obj3 = {obj1,obj2}
+//const obj3 =Object.assign({},obj1,obj2,obj4)
+const obj3={...obj1,...obj2}
+//console.log(obj3);
+const users = [
+    {
+        id: 1,
+        email: "r@gmail.com"
+    },
+    {
+        id: 2,
+        email: "s@gmail.com"
+    }
+]
+//console.log(users[0].email); // Output: "r@gmail.com"
+//console.log(users[1].email); // Output: "s@gmail.com"
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwenproperty('isLoggedIn'));
