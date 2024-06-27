6935746ab572d5bd07dda8454caa853d89a48fde
Rakshitson1410 <rakshitrsoni@gmail.com>
2024-06-28 00:42:41 +0530

    some changes

---
+++ loops2.js
@@ -0,0 +1,35 @@
+// for of 
+
+
+const arr=[3,5,6,7]
+
+//for (const i  of arr) {
+  //  console.log(i);
+//}/
+
+//const greetings= "hello crush"
+//for (const i of greetings) {
+ //   console.log(i);
+//}
+
+// maps
+
+const map = new Map()
+map.set('IN',"india")
+map.set('us',"usa")
+map.set('aus',"austalia")
+
+/*for (const [key,value] of map) {
+    console.log(key,':-',value);
+}*/
+
+//const myobject = {
+   // game1 :'COD',
+    //game :'ff',
+
+//}
+//for (const [key,value] of myobject) {
+  //  console.log(key, ':-',value);
+//}
+
+ 
