bf7af10e4a3f8e5e799358aea91df8809fbeb905
Rakshitson1410 <rakshitrsoni@gmail.com>
2024-12-17 12:36:50 +0530

    ok

---
+++ .vscode/promoices.js
@@ -0,0 +1,12 @@
+const PromiseOne = new Promise(function (resolve, reject) {
+    // do async task 
+    // db call, cryptography, network call etc
+    setTimeout(function() {
+        console.log('Async task is complete');
+        resolve(); // Call resolve to fulfill the promise
+    }, 1000);
+});
+
+PromiseOne.then(function() {
+    console.log("promise consumed");
+});
